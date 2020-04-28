5b6a89c58186530c0c48a74335beb5faa394f4fc
Nishan Chathuranga <inbox.nishan@gmail.com>
2020-04-28 23:16:29 +0530

    connected amplitude of a song instead of a mic

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,37 @@
+var song;
+var buttton;
+var amp; //volume
+
+function toggleSong() {
+  if(song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+function preload() {
+  song = loadSound('audio/the-alphabeat.mp3'); 
+  getAudioContext().resume();
+}
+
 function setup() {
-  createCanvas(200, 200)
+  createCanvas(600, 600)
+  buttton = createButton('Toggle');
+  buttton.mousePressed(toggleSong);
+  song.play();  
+  amp = new p5.Amplitude();
 }
 
 function draw() {
-  background(51);
-  ellipse(mouseX, mouseY, 60, 60);
+  background(0);
+  var vol = amp.getLevel();
+  ellipse(300, 300, vol*300, vol*300);
+  console.log(vol)
+}
+
+// Chrome 70 will require user gestures required to enable web audio api
+// Click on the web page to start audio
+function touchStarted() {
+  getAudioContext().resume();
 }
