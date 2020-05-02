a4b6a7ab5137f4609a01ad627b5b16d2cb7c6d19
Nishan Chathuranga <inbox.nishan@gmail.com>
2020-05-02 21:49:31 +0530

    line spectrum

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,4 @@
-let song, buttton, amp;
-let volHistory = [];
+let song, buttton, fft, space_between_lines;
 
 function toggleSong() {
   if(song.isPlaying()) {
@@ -11,44 +10,31 @@ function toggleSong() {
 
 function preload() {
   song = loadSound('audio/the-alphabeat.mp3'); 
-  getAudioContext().resume();
 }
 
 function setup() {
   createCanvas(600, 600);
   angleMode(DEGREES); // Change the mode to DEGREES
-  buttton = createButton('Toggle');
+  buttton = createButton('Toggle Play');
   buttton.mousePressed(toggleSong);
   song.play();  
-  amp = new p5.Amplitude();
+  fft = new p5.FFT(0.9, 128);
+  space_between_lines = width / 128;
 }
 
 function draw() {
   background(0);
-  let vol = amp.getLevel();
-  volHistory.push(vol);
-
-  translate(width/2, height/2)
-  noFill();
-  beginShape();
-  for (let i = 0; i < 360; i++) {
-    stroke(255);
-    let r = map(volHistory[i], 0, 1, 10, 300);
-    let x = r * cos(i);
-    let y = r * sin(i);
-    vertex(x, y);
-  }
-  endShape();
-
-  if(volHistory.length > 360) {
-    volHistory.splice(0,1);
-  }
   
-  // ellipse(300, 300, vol*300, vol*300);
-  console.log(vol)
+  let spectrum = fft.analyze();
+  for (let i = 0; i < spectrum.length; i++) {
+    stroke(255);
+    let amp = spectrum[i];
+    let y = map(amp, 0, 256, height, 0);
+    line(i * space_between_lines, height, i * space_between_lines, y);
+  }
 }
 
-// Chrome 70 will require user gestures required to enable web audio api
+// Chrome 70 will require user gestures to enable web audio api
 // Click on the web page to start audio
 function touchStarted() {
   getAudioContext().resume();
